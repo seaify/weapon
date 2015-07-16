fd7f160494a22d1c0b321fa08e8554505cb6e0ed
seaify <dilin.life@gmail.com>
2015-07-16 21:48:54 +0800

    update

--- lib/weapon.rb
+++ lib/weapon.rb
@@ -1,15 +1,38 @@
 require 'thor'
-#Hello weapon
+
 class Weapon < Thor
-  #used to test love
-  attr_reader :love
-  def self.hi(ff)
-    puts "Hello world!"
-    puts ff
+
+  desc "setup_mina_deploy", "setup mina deploy"
+  def setup_mina_deploy
+    puts "setup mina deploy"
   end
 
-  def what
-    puts "what"
+  desc "setup_settings_ui", "setup settings ui"
+  def setup_settings_ui
+    puts "setup settings ui"
+  end
+
+  desc "setup_exception_slack_notify", "setup exception slack notify"
+  def setup_exception_slack_notify
+    puts "setup exception slack notify"
+  end
+
+
+  desc "custom_i18n", "custom i18n"
+  def custom_i18n
+    puts "custom i18n"
+  end
+
+  desc "install_recomend_gems", "install_recommend_gems"
+  def install_recommend_gems
+    puts "install recommend gems"
+  end
+
+  desc "pull_to_github", "pull to github"
+  def pull_to_github
+    puts "pull to github"
   end
 
 end
+
+Weapon.start ARGV
