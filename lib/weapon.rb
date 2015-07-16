8abc4abc38d55d7fcc8f1f094939b2f241075efb
seaify <dilin.life@gmail.com>
2015-07-16 20:50:25 +0800

    update

--- lib/weapon.rb
+++ lib/weapon.rb
@@ -1,9 +1,15 @@
 require 'thor'
-
+#Hello weapon
 class Weapon < Thor
+  #used to test love
+  attr_reader :love
   def self.hi(ff)
     puts "Hello world!"
     puts ff
+  end
+
+  def what
+    puts "what"
   end
 
 end
